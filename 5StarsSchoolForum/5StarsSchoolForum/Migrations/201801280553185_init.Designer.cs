009e00a0c6aac4db8f1b9f7df292ee5cf8aee3ad
mosh <moshenahmed@gmail.com>
2018-01-29 12:09:07 +0100

    working on replyy and message

--- 5StarsSchoolForum/5StarsSchoolForum/Migrations/201801280553185_init.Designer.cs
+++ 5StarsSchoolForum/5StarsSchoolForum/Migrations/201801280553185_init.Designer.cs
@@ -13,7 +13,7 @@ namespace _5StarsSchoolForum.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201801271019054_init"; }
+            get { return "201801280553185_init"; }
         }
         
         string IMigrationMetadata.Source
